2250ecff3c102c3e8d5a9f95a2834a1454070996
Viktor Yankovskyi <viktor.yankovskyi@windmill.ch>
2022-07-11 14:46:38 +0200

    getStaticPath

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -5,6 +5,7 @@ import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 import { FunctionComponent } from 'react';
+import React from 'react';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
